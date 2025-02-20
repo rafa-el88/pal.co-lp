059c99059dd2a9e8a646327d9bcd659c3218a858
Rafael Rodrigues <rafaa.cfc@gmail.com>
2025-02-20 17:29:54 -0300

    fix modal, add close modal, fix layout

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ let currentSlide = 1;
 const totalSlides = 3;
 
 function showSlide(slideIndex) {
-  for (let i = 1; i <= totalSlides; i++) {
-    document.getElementById(`id_slide_${i}`).style.display = i === slideIndex ? 'inline-flex' : 'none';
-    document.getElementById(`button_slider_${i}`).classList.toggle('active', i === slideIndex);
-    document.getElementById(`button_slider_${i + 3}`).classList.toggle('active', i === slideIndex);
-    document.getElementById(`button_slider_${i + 6}`).classList.toggle('active', i === slideIndex);
-  }
+    for (let i = 1; i <= totalSlides; i++) {
+        document.getElementById(`id_slide_${i}`).style.display = i === slideIndex ? 'inline-flex' : 'none';
+        document.getElementById(`button_slider_${i}`).classList.toggle('active', i === slideIndex);
+        document.getElementById(`button_slider_${i + 3}`).classList.toggle('active', i === slideIndex);
+        document.getElementById(`button_slider_${i + 6}`).classList.toggle('active', i === slideIndex);
+    }
 }
 
 function nextSlide() {
-  currentSlide = (currentSlide % totalSlides) + 1;
-  showSlide(currentSlide);
+    currentSlide = (currentSlide % totalSlides) + 1;
+    showSlide(currentSlide);
 }
 
-setInterval(nextSlide, 6000); // Change slide every 10 seconds
+setInterval(nextSlide, 6000); // Change slide every 6 seconds
 
 // Initialize the first slide
 showSlide(currentSlide);
@@ -35,22 +35,32 @@ document.getElementById('button_slider_9').addEventListener('click', () => showS
 
 // Add click event listeners for minus buttons
 document.querySelectorAll('.minus-btn').forEach(button => {
-  button.addEventListener('click', () => {
-    const pTag = button.closest('.pergunta').querySelector('.div-11');
-    pTag.style.display = pTag.style.display === 'none' ? 'block' : 'none';
-  });
+    button.addEventListener('click', () => {
+        const pTag = button.closest('.pergunta').querySelector('.div-11');
+        pTag.style.display = pTag.style.display === 'none' ? 'block' : 'none';
+    });
 });
 
 // Add click event listeners for plus buttons
 document.querySelectorAll('.plus-btn').forEach(button => {
-  button.addEventListener('click', () => {
-    const pTag = button.closest('.pergunta').querySelector('.div-11');
-    const imgTag = button.querySelector('img');
-    const isHidden = pTag.style.display === 'none';
-    pTag.style.display = isHidden ? 'block' : 'none';
-    imgTag.src = isHidden ? 'img/fi-rr-minus-small-11.svg' : 'img/fi_plus.svg';
-  });
+    button.addEventListener('click', () => {
+        const pTag = button.closest('.pergunta').querySelector('.div-11');
+        const imgTag = button.querySelector('img');
+        const isHidden = pTag.style.display === 'none';
+        pTag.style.display = isHidden ? 'block' : 'none';
+        imgTag.src = isHidden ? 'img/fi-rr-minus-small-11.svg' : 'img/fi_plus.svg';
+    });
 });
+
+
+// document.querySelectorAll('.button-modal').forEach(button => {
+//     button.addEventListener('click', () => {
+//         const modal = document.querySelector('#overlay-lead');
+//         modal.classList.toggle('hide');
+//     });
+// });
+
+
 
 const abrirModalBotao = document.querySelector("#abrir-modal");
 const fecharModalButton = document.querySelector("#fechar-modal");
@@ -58,9 +68,9 @@ const modal = document.querySelector("#overlay-lead");
 const fade = document.querySelector("#fade");
 
 const alternarModal = () => {
-  [modal, fade].forEach((el) => el.classList.toggle("hide"));
+    [modal, fade].forEach((el) => el.classList.toggle("hide"));
 };
 
 [abrirModalBotao, fecharModalButton, fade].forEach((el) => {
-  el.addEventListener("click", () => alternarModal());
+    el.addEventListener("click", () => alternarModal());
 });
